9d663265af0ab440bbf13155c9821fdb005bdf33
John Weaver <john@saebyn.info>
2013-08-19 13:29:45 -0700

    Removes environ argument from method not called by plugin.

--- repoze/who/plugins/oauth/managers.py
+++ repoze/who/plugins/oauth/managers.py
@@ -132,7 +132,7 @@ class DefaultManager(object):
         token = tokens.first()
         return token
 
-    def set_request_token_user(self, key, userid, environ):
+    def set_request_token_user(self, key, userid):
         r"""Register the user id for this token and also generate a verification
         code."""
         token = self.get_request_token(key)
