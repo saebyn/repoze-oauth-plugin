a8bd0baa3ffa8531ddbb9d47906a1a74ca9f3394
John Weaver <john@saebyn.info>
2013-09-03 12:14:00 -0700

    Bumps version number.

--- setup.py
+++ setup.py
@@ -6,19 +6,22 @@ import sys
 # Fool distutils to accept more than ASCII
 reload(sys).setdefaultencoding('utf-8')
 
-version = '0.2-flex'
+version = '0.3.0-saltbox'
 
-setup(name='repoze-oauth-plugin',
+setup(
+    name='repoze-oauth-plugin',
     version=version,
     description='OAuth plugin for repoze.who and repoze.what',
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
     keywords='auth repoze repoze.who repoze.what predicate oauth',
     author=u'Linas Juškevičius',
     author_email='linas.juskevicius@gmail.com',
     license='MIT',
     packages=find_packages(exclude=['tests']),
-    namespace_packages=['repoze', 'repoze.who', 'repoze.who.plugins',
+    namespace_packages=[
+        'repoze', 'repoze.who', 'repoze.who.plugins',
         'repoze.what', 'repoze.what.plugins'],
     include_package_data=True,
     zip_safe=False,
