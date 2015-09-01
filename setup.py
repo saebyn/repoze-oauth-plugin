0b5ab3e98374d43df14480c575bcdd2b8434e492
John Weaver <john@pledge4code.com>
2015-09-01 02:37:19 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='repoze-oauth-plugin',
     zip_safe=False,
     test_suite='nose.collector',
     dependency_links=[
-        'https://github.com/saebyn/repoze.who/tarball/ed6d1c826786e8f2f25441df6b13b34e15367d0b#egg=repoze.who-1.0.20',
+        'https://bitbucket.org/saebyn/repoze.who/get/7b6211e2e598.zip#egg=repoze.who-1.0.20',
     ],
     install_requires=[
         'repoze.who==1.0.20',
